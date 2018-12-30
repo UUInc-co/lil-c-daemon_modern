71ab19c36dbdfa497621b02067d04727168117de
S//J UU <youdouble@users.noreply.github.com>
2018-12-29 21:33:50 -0800

    Init

---
+++ src/lilcdaemon_modern.c
@@ -0,0 +1,10 @@
+/*****************************************************************************\
+*
+*   Name:           
+*   Source:         https://github.com/youdouble/lil-c-daemon_modern
+*   License:        MIT (See Repo)
+*   Copyright:      (C) 2018 Joshua Whittington
+*   Contact:        lilc_modern@doubleyoudouble.me
+*   Use:            Daemonize small programs or scripts
+*
+\*****************************************************************************/
